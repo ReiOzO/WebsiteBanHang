f71684417fb8344a99b050accbf1ff9f0121ef39
Rei ozo <zin21022003@gmail.com>
2024-03-31 16:25:29 +0700

    THLTW Buoi4

---
+++ WebsiteBanHang/Controllers/CategoryController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebsiteBanHang.Models;
+
+namespace WebsiteBanHang.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryRepository.GetAllAsync();
+            return View(categories);
+        }
+
+        public async Task<IActionResult> Add()
+        {
+            var categories = await _categoryRepository.GetAllAsync();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Category category)
+        {
+            await _categoryRepository.AddAsync(category);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Display(int id)
+        {
+            var category = await _categoryRepository.GetByIdAsync(id);
+            return View(category);
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            var category = await _categoryRepository.GetByIdAsync(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, Category category)
+        {
+            await _categoryRepository.UpdateAsync(category);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var category = await _categoryRepository.GetByIdAsync(id);
+            return View(category);
+        }
+
+        [HttpPost,]
+        public async Task<IActionResult> Delete(int id, Category category)
+        {
+            if (id != category.Id)
+            {
+                return NotFound();
+            }
+            await _categoryRepository.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
